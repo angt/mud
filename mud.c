f67eae0a7f536d6593217f1949d0efa21a2e0b7a
Adrien Gallouët <adrien@gallouet.fr>
2017-01-20 09:52:28 +0000

    Restore old behaviour

--- mud.c
+++ mud.c
@@ -1240,10 +1240,11 @@ mud_send(struct mud *mud, const void *data, size_t size, int tc)
             limit = path->rtt / 2;
         }
 
-        if (mud_timeout(now, path->recv_time, mud->send_timeout + MUD_ONE_SEC) &&
-            mud_timeout(now, path->send_time, mud->send_timeout)) {
-            mud_send_path(mud, path, now, packet, packet_size, tc);
-            path->limit = limit;
+        if (mud_timeout(now, path->recv_time, mud->send_timeout + MUD_ONE_SEC)) {
+            if (mud_timeout(now, path->send_time, mud->send_timeout)) {
+                mud_send_path(mud, path, now, packet, packet_size, tc);
+                path->limit = limit;
+            }
             continue;
         }
 
