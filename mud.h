03be90b6067937e3ab88e2aea96ca0dd1e910d16
Adrien Gallouët <adrien@gallouet.fr>
2018-06-05 13:56:15 +0000

    Update mud.h
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- mud.h
+++ mud.h
@@ -70,6 +70,8 @@ int mud_get_key (struct mud *, unsigned char *, size_t *);
 void   mud_set_mtu (struct mud *, size_t);
 size_t mud_get_mtu (struct mud *);
 
+unsigned long mud_get_sync_elapsed_msec (struct mud *);
+
 int mud_set_send_timeout   (struct mud *, unsigned long);
 int mud_set_time_tolerance (struct mud *, unsigned long);
 int mud_set_keyx_timeout   (struct mud *, unsigned long);
